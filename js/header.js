824b98ab7559207b8416a8111850f61c841ac485
Sathwik Edupuganti <sathwik.edupuganti@gmail.com>
2019-06-23 17:30:24 +0530

    changes made

---
+++ js/header.js
@@ -0,0 +1,17 @@
+function SignIn() {
+    document.getElementById('signInModal').style.display='block';
+}
+    
+function signUp() {
+    document.getElementById('signUpBtnModal').style.display='block';
+}
+
+function onCloseModalClicked() {
+    document.getElementById('signInModal').style.display='none';
+    document.getElementById('signUpBtnModal').style.display='none';
+}
+
+function opensignUpBtnButton(){
+    document.getElementById('signUpBtnModal').style.display='block';
+}
+    
